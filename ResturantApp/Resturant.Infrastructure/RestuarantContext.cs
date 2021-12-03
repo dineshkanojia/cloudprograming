28280c710fde169c7fc37684f5754ad74afe6e96
DineshKanojiya <dinesh_kanojia3@yahoo.com>
2021-12-03 19:14:54 +0530

    ResturantApp Added to GitHUb

---
+++ ResturantApp/Resturant.Infrastructure/RestuarantContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Resturant.Infrastructure
+{
+    public class RestuarantContext : DbContext
+    {
+        public RestuarantContext(DbContextOptions<RestuarantContext> options) : base(options)
+        {
+            
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+        }
+
+    }
+}
---
+++ ResturantApp/ResturantApp/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using ResturantApp.Areas.Identity.Data;
+using ResturantApp.Data;
+
+[assembly: HostingStartup(typeof(ResturantApp.Areas.Identity.IdentityHostingStartup))]
+namespace ResturantApp.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) =>
+            {
+                services.AddDbContext<ResturantAppDBContext>(options =>
+                    options.UseSqlServer(
+                        context.Configuration.GetConnectionString("ResturantAppDBContextConnection")));
+
+                services.AddDefaultIdentity<ResturantAppUser>(options =>
+                {
+                    options.SignIn.RequireConfirmedAccount = false;
+                    options.Password.RequireUppercase = false;
+                    options.Password.RequireLowercase = false;
+                })
+                    .AddEntityFrameworkStores<ResturantAppDBContext>();
+            });
+        }
+    }
+}
